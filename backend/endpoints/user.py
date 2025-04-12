4283db29b94529178538416bf76c792ca7d5df30
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2025-04-12 00:09:09 -0300

    Criando endpoints do user e funções para hash de senha

---
+++ backend/endpoints/user.py
@@ -0,0 +1,57 @@
+import jwt
+from fastapi import APIRouter, HTTPException, Depends
+from models import *
+from typing import Annotated
+from sqlmodel import Session, select
+from database import get_session
+from hashfunctions import get_password_hash, verify_password
+
+SessionDep = Annotated[Session, Depends(get_session)]
+
+router = APIRouter()
+key = "trans@ctIOnAPP"
+
+@router.post("/registrar", status_code=201)
+def create_user(user: UserCreate, session: SessionDep):
+
+    # Verifica se o email já está no banco de dados
+    select_user = select(User).where(User.email == user.email)
+    existent_user = session.exec(select_user).all()
+
+    if not existent_user:
+        hashed_password = get_password_hash(user.password) # Faz hash da senha
+        new_user = User(
+            email = user.email,
+            name = user.name,
+            password = hashed_password,
+            birthDate = user.birthDate
+        )
+        session.add(new_user) # cria novo user
+        session.commit()
+        session.refresh(new_user)
+
+        # Cria jwt
+        payload = {"email": new_user.email}
+        encoded_jwt = jwt.encode(payload, key, algorithm="HS256")
+        json_jwt = {"jwt": f"{encoded_jwt}"}
+        return json_jwt
+    else:
+        # Se o email já está cadastrado
+        raise HTTPException(status_code=409, detail=f"Email já cadastrado")
+
+@router.post("/login", status_code=200)
+def login(login: Login, session: SessionDep):
+    # Busca usuário no banco de dados
+    existent_user = session.scalar(select(User).where(User.email == login.email))
+
+    if existent_user:
+        hashed_password = existent_user.password
+        if (verify_password(login.password, hashed_password)):
+            payload = {"email" : existent_user.email}
+            encoded_jwt = jwt.encode(payload, key, algorithm="HS256")
+            json_jwt = {"jwt": f"{encoded_jwt}"}
+            return json_jwt
+        else:
+            raise HTTPException(status_code=401, detail=f"Senha incorreta")
+    else:
+        raise HTTPException(status_code=401, detail=f"Usuário não cadastrado")
--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,7 @@
 from fastapi import FastAPI
 from database import lifespan
+from endpoints import user
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(user.router)
