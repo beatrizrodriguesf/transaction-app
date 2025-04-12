4b31d180e21b4d283144d0be67e9d0b94987950b
beatrizrodriguesf <beatrizrf1@al.insper.edu.br>
2025-04-12 13:35:39 -0300

    Separando as funções do token e mudando de página após o login

--- frontend/transaction-app/src/App.js
+++ frontend/transaction-app/src/App.js
@@ -1,23 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import Login from './components/login/Login'
-// import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-function setToken(userToken) {
-  sessionStorage.setItem('token', JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem('token');
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
+import useToken from './useToken';
 
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if(!token) {
-    return <Login setToken={ setToken } />
+    return <Login setToken={setToken} />
   }
 
   return (
